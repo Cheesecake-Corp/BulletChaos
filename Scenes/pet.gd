5bb4d19db72b5435eee1446615fe188de8bfd052
Hogofogo0 <68776905+Hogofogo0@users.noreply.github.com>
2026-02-08 12:22:33 +0100

    made camera smoother

--- Scenes/pet.gd
+++ Scenes/pet.gd
@@ -1,8 +1,10 @@
 extends Sprite2D
 
 var PET_SPEED = 50
-@onready var camera_2d: Camera2D = $"../Player/Camera2D"
-@onready var player: CharacterBody2D = $"../Player"
+@onready var player: CharacterBody2D = $"../.."
+@onready var camera_2d: Camera2D = $".."
+
 # Called when the node enters the scene tree for the first time.
 func _physics_process(_delta: float) -> void:
-	global_position = camera_2d.get_screen_center_position() + (global_position-player.global_position)/3
+	global_position = camera_2d.get_screen_center_position() + (player.global_position)/5
+	global_position.y -= 12
