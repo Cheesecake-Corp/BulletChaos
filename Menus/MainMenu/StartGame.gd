9f5a0eb311025cdd246632e63ca2cf02aa2266d3
vyuka <vyuka@UC333P03-KIV.wxk.zcu.cz>
2026-02-27 08:55:23 +0100

    gen change

--- Menus/MainMenu/StartGame.gd
+++ Menus/MainMenu/StartGame.gd
@@ -7,6 +7,6 @@ func _ready() -> void:
 
 
 func _pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/ExampleLvl.tscn")
+	get_tree().change_scene_to_file("res://World/ExampleLvl.tscn")
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 # Called every frame. 'delta' is the elapsed time since the previous frame.
