f4abae56ddc0e5a3096f1fc89daf16b890a40ec0
hogofogo0 <hogofogo0@cheesecakecorp.org>
2026-04-02 22:55:26 +0200

    changes to not kill my RAM

--- World/Generation/maptest.gd
+++ World/Generation/maptest.gd
@@ -18,7 +18,7 @@ func _ready() -> void:
 	pass
 
 func start(maxcount: int):
-	GAME.change_seed("LdddddddddddddggadadggggddgO")
+	GAME.change_seed("Schokolate")
 	var spawn: Room = Spawn_scene[GAME.RANDOM_GENERATION.randi_range(0,Spawn_scene.size()-1)].instantiate()
 	add_child(spawn)
 	#get_tree().current_scene.call_deferred("add_child", spawn)
@@ -163,4 +163,4 @@ func write(room: Room, coordinates: Vector2i, exit_entry: Exit, exit_exit: Exit)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if(Input.is_action_just_pressed("generate")):
-		start(250)
+		start(50)
