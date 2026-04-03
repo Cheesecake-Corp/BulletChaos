d71960202b16b3350f8a6186bf1029b547d89728
hogofogo0 <hogofogo0@cheesecakecorp.org>
2026-04-03 11:10:57 +0200

    made a sample room

--- World/ChallangeRoom/challange_room.gd
+++ World/ChallangeRoom/challange_room.gd
@@ -3,9 +3,9 @@ func _init() -> void:
 	size = {}
 	size[0] = {}
 	size[0]["position"] = Vector2i(0,0)
-	size[0]["size"] = Vector2i(21,14)
+	size[0]["size"] = Vector2i(32,20)
 	 
-	exits.append(Exit.new().set_location(Vector2i(9,0)).set_direction(direction.TOP).set_room(self))
-	exits.append(Exit.new().set_location(Vector2i(0,7)).set_direction(direction.LEFT).set_room(self))
-	exits.append(Exit.new().set_location(Vector2i(8,14)).set_direction(direction.DOWN).set_room(self))
-	exits.append(Exit.new().set_location(Vector2i(21,8)).set_direction(direction.RIGHT).set_room(self))
+	exits.append(Exit.new().set_location(Vector2i(13,0)).set_direction(direction.TOP).set_room(self))
+	exits.append(Exit.new().set_location(Vector2i(0,4)).set_direction(direction.LEFT).set_room(self))
+	exits.append(Exit.new().set_location(Vector2i(13,20)).set_direction(direction.DOWN).set_room(self))
+	exits.append(Exit.new().set_location(Vector2i(32,4)).set_direction(direction.RIGHT).set_room(self))
