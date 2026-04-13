7cd9cb35d671f036da013323a3152a2b82ca72f7
hogofogo0 <hogofogo0@cheesecakecorp.org>
2026-04-13 16:17:11 +0200

    made physics process less nested for pet

--- Player/Pet/pet.gd
+++ Player/Pet/pet.gd
@@ -7,8 +7,8 @@ class_name Pet
 
 var boss_room := GAME.boss_room_pos
 const SPEED = 100
-var moving := false
-var search := false
+@export var moving := false
+@export var search := false
 
 func _ready() -> void:
 	region.top_level = true
@@ -25,41 +25,63 @@ func bake_nav() -> void:
 	region.bake_navigation_polygon()
 
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("pet", false): #Player pressed B
-		if moving == false:
-			sprite.play("search")
-			search = true
-			nav.target_position = boss_room
-			moving = true
-		else: #Stops searching for boss room
-			moving = false
+func _physics_process(delta: float) -> void:
+	_handle_pet_input()
+	_handle_movement(delta)
 
+
+func _handle_pet_input() -> void:
+	if not Input.is_action_just_pressed("pet", false):
+		return
 	if moving:
-		if nav.is_navigation_finished():
-			moving = false
-			sprite.play("idle")
-		elif search == false:
-			sprite.play("walk")
-			var next := nav.get_next_path_position()
-			var direction := (next - global_position).normalized()
-			if direction.x < 0:
-				sprite.flip_h = true
-			else:
-				sprite.flip_h = false
-			global_position += direction * SPEED * delta
-			
-	elif (global_position - GAME.player.global_position).abs() > Vector2(50,50): #Follows player
-		nav.target_position = GAME.player.global_position
-		var direction := (nav.get_next_path_position() - global_position).normalized()
-		sprite.play("walk")
-		if direction.x < 0:
-			sprite.flip_h = true
-		else:
-			sprite.flip_h = false
-		global_position += direction * SPEED * delta
+		moving = false
+	else:
+		sprite.play("search")
+		search = true
+		nav.target_position = boss_room
+		moving = true
+
+
+func _handle_movement(delta: float) -> void:
+	if moving:
+		_handle_moving_state(delta)
+	elif global_position.distance_to(GAME.player.global_position) > 50:
+		_follow_player(delta)
 	else:
 		sprite.play("idle")
+
+
+func _handle_moving_state(delta: float) -> void:
+	if nav.is_navigation_finished():
+		moving = false
+		sprite.play("idle")
+		return
+
+	if search:
+		return
+
+	var dist := global_position.distance_to(GAME.player.global_position)
+	if dist > 400:
+		moving = false
+		return
+	if dist > 250:
+		sprite.play("idle")
+		return
+
+	_move_along_path(delta)
+
+
+func _follow_player(delta: float) -> void:
+	nav.target_position = GAME.player.global_position
+	sprite.play("walk")
+	_move_along_path(delta)
+
+
+func _move_along_path(delta: float) -> void:
+	var next := nav.get_next_path_position()
+	var direction := (next - global_position).normalized()
+	sprite.flip_h = direction.x < 0
+	global_position += direction * SPEED * delta
 
 
 func _on_animated_sprite_2d_animation_finished() -> void: #Search animation finished
